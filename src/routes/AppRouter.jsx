2052b679f61685724eebc27f4d7a1f4579cd8c5d
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-02-09 15:54:45 -0300

    Agrega en AppRouter [routes] las rutas de navegacion

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+
+//Components
 import NavBar from '../components/NavBar';
 
-//<Route exact path='/' element={"<Component>"}/>
+//Pages
+import AgregarScreen from '../pages/AgregarScreen';
+import BuscarScreen from '../pages/BuscarScreen';
+import EditarScreen from '../pages/EditarScreen';
+import InicioScreen from '../pages/InicioScreen';
 
 const AppRouter = () =>
 {
     return(
 		<Router>
             <NavBar/>            
-			<Routes>				
+			<Routes>
+			<Route end path='/' element={<InicioScreen/>}/>				
+				<Route end path='/buscar' element={<BuscarScreen/>}/>
+				<Route end path='/editar' element={<EditarScreen/>}/>
+				<Route end path='/agregar' element={<AgregarScreen/>}/>
+				<Route path='*' element={<Navigate to='/'/>}/>				
 			</Routes>
 		</Router>
     );
