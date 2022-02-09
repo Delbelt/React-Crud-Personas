4301ebf055defcc2e72a6b49296de0caa8dd108f
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-02-09 15:39:24 -0300

    Agrega al App.js el componente AppRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import AppRouter from './routes/AppRouter'
 
 const App = () =>
 {
     return(
         <React.Fragment>
-
+            <AppRouter/>
         </React.Fragment>
     );
 }
