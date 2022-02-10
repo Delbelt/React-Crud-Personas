6f310ef6c2ad319652dc90a20f15e7d4d272ab3f
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-02-10 14:37:12 -0300

    Modifica el formulario [components] y sus elementos

--- src/components/FormAdd.jsx
+++ src/components/FormAdd.jsx
@@ -5,12 +5,17 @@ const FormAdd = () =>
 {
     return(
         <React.Fragment>
-            <h1>Formulario</h1>
 
-            <Input tipo='text' label='Nombre' placeholder='Ingrese su nombre'
-            name='nombre' error='Solo puede ingresar letras'/>
-            <Input tipo='text' label='Dni' placeholder='Ingrese su Dni'
-            name='dni' error='Solo puede ingresar 8 digitos'/>
+            <form action="">
+                <div>
+                    <Input tipo='text' label='Nombre' placeholder='Ingrese su nombre'
+                    name='nombre' error='Solo puede ingresar letras'/>
+                    <Input tipo='text' label='Dni' placeholder='Ingrese su Dni'
+                    name='dni' error='Solo puede ingresar 8 digitos'/>
+                    <button>Agregar Persona</button>
+                </div>                            
+            </form>
+            
 
         </React.Fragment>
     );
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Input = ({tipo, label, placeholder, name, error}) =>
 {
     return(
-        <React.Fragment>
+        <React.Fragment>            
             <label htmlFor={name}>{label}</label>
-            <input type={tipo} placeholder={placeholder} id={name}/>
+            <input autoComplete='off' type={tipo} placeholder={placeholder} id={name}/>
             <p>{error}</p>
         </React.Fragment>
     );
