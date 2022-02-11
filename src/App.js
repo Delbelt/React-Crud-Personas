45333f3418cc7b677ff7686bcc05566f2da6eef5
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-02-11 13:01:05 -0300

    Implementa el Context y el reducer en App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
+import PersonaContext from './context/PersonaContext';
+import PersonaReducer from './reducer/PersonaReducer';
 import AppRouter from './routes/AppRouter'
+
+const init = () =>
+{
+    const clientes = localStorage.getItem("personas");
+    return clientes ? JSON.parse(clientes) : [];
+}
 
 const App = () =>
 {
+    const [state, dispatch] = useReducer(PersonaReducer, [], init); //El reducer
+
+    useEffect(() => //Se lanza cada vez que agreguemos o eliminemos clientes [state]
+    {
+        localStorage.setItem("personas", JSON.stringify(state)) //convertimos los datos a String
+        //Recibe dos parametros: (key: String, value: String)
+    },[state]);
+
     return(
-        <React.Fragment>
+        <PersonaContext.Provider value={{state, dispatch}}>
             <AppRouter/>
-        </React.Fragment>
+        </PersonaContext.Provider>        
     );
 }
 
